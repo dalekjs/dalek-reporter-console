376d5d1d7bc8f7814fa8d13b1a379a8816078427
asciidisco <public@asciidisco.com>
2013-06-23 15:02:40 +0200

    Added mocha test boilerplate

---
+++ test/index_TEST.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('dalek-reporter-console', function() {
+
+  it('should get default level', function(){
+    var ConsoleResporter = require('../index')({events: {on: function () {}, off: function () {}}});
+    expect(ConsoleResporter.level).to.equal(1);
+  });
+
+});
