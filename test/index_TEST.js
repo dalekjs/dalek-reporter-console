a7eac78b55d7597d5d26c5ec12983d2352bc2c45
mdix <dix@mediaman.de>
2014-03-16 08:33:53 +0100

    Fixed typo

--- test/index_TEST.js
+++ test/index_TEST.js
@@ -5,8 +5,8 @@ var expect = require('chai').expect;
 describe('dalek-reporter-console', function() {
 
   it('should get default level', function(){
-    var ConsoleResporter = require('../index')({config: {config: {}}, events: {on: function () {}, off: function () {}}});
-    expect(ConsoleResporter.level).to.equal(1);
+    var ConsoleReporter = require('../index')({config: {config: {}}, events: {on: function () {}, off: function () {}}});
+    expect(ConsoleReporter.level).to.equal(1);
   });
 
 });
